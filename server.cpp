d3a17c8934fac36d7e9dc914c509d7e5aa757c9e
Kevin Kee <kevinkee9@gmail.com>
2021-02-23 21:51:52 +0900

    server: Fix spacing (4 -> 2)

--- server.cpp
+++ server.cpp
@@ -10,60 +10,60 @@
 using namespace std;
 
 void server() {
-    int MAXLINE = 1024;
-    int PORT = 8080;
+  int MAXLINE = 1024;
+  int PORT = 8080;
 
-    char buffer[MAXLINE];
-    struct sockaddr_in server_addr;
-    struct sockaddr_in client_addr;
+  char buffer[MAXLINE];
+  struct sockaddr_in server_addr;
+  struct sockaddr_in client_addr;
 
-    socklen_t size;
+  socklen_t size;
 
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
-    if (sockfd < 0) {
-        cout << "\nError establishing socket" << endl;
-        exit(1);
-    }
+  if (sockfd < 0) {
+    cout << "\nError establishing socket" << endl;
+    exit(1);
+  }
 
-    cout << "\nSocket created" << endl;
+  cout << "\nSocket created" << endl;
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htons(INADDR_ANY);
-    server_addr.sin_port = htons(PORT);
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = htons(INADDR_ANY);
+  server_addr.sin_port = htons(PORT);
 
-    int rbind = bind(
-        sockfd, (struct sockaddr*) &server_addr, sizeof(server_addr));
-    
-    if (rbind < 0) {
-        cout << "\nCould not bind: " << strerror(errno) << endl;
-        exit(1);
-    }
+  int rbind = bind(
+    sockfd, (struct sockaddr*) &server_addr, sizeof(server_addr));
+  
+  if (rbind < 0) {
+    cout << "\nCould not bind: " << strerror(errno) << endl;
+    exit(1);
+  }
 
-    cout <<"\nSocket bound" << endl;
+  cout <<"\nSocket bound" << endl;
 
-    while (true) {
-        socklen_t addr_len = sizeof(client_addr);
-        int nbytes = recvfrom(
-            sockfd,
-            (char *)buffer,
-            MAXLINE,
-            MSG_WAITALL,
-            (struct sockaddr *) &client_addr,
-            &addr_len
-        );
+  while (true) {
+    socklen_t addr_len = sizeof(client_addr);
+    int nbytes = recvfrom(
+      sockfd,
+      (char *)buffer,
+      MAXLINE,
+      MSG_WAITALL,
+      (struct sockaddr *) &client_addr,
+      &addr_len
+    );
 
-        cout << "\nClient: " << buffer << endl;
+    cout << "\nClient: " << buffer << endl;
 
-        sendto(
-            sockfd,
-            buffer,
-            nbytes,
-            MSG_CONFIRM,
-            (struct sockaddr *) &client_addr,
-            addr_len
-        );
+    sendto(
+      sockfd,
+      buffer,
+      nbytes,
+      MSG_CONFIRM,
+      (struct sockaddr *) &client_addr,
+      addr_len
+    );
 
-        cout << "\nResponse sent" << endl;
-    }
+    cout << "\nResponse sent" << endl;
+  }
 }
